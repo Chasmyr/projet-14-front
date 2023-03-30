a2d0e7ee4fd3b3ed7fee5dbb12b7f8702b6f1ad5
gabin <lestratgabin@gmail.com>
2023-03-30 15:32:00 +0200

    header fix

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,13 +5,13 @@ import './index.css'
 const Header = () => {
 
     return (
-        <Box sx={{ mb: 15 }}>
+        <Box sx={{ mb: 12 }}>
             <AppBar>
                 <Toolbar sx={{ display: 'flex', m: 1 }}>
                     <Typography variant="h1" sx={{ fontSize: {xs: '38px', md:'48px'} }}>
                         HRnet
                     </Typography>
-                    <Box sx={{ width: {xs:'200px', sm: '300px', md:'350px'}, display: 'flex', justifyContent: 'space-between', ml: {xs: 4, sm: 6, md: 8} }}>
+                    <Box sx={{ width: {xs:'250px', sm: '350px'}, display: 'flex', justifyContent: 'space-between', ml: {xs: 4, sm: 6, md: 8} }}>
                         <Link to="/">Add Employee</Link>
                         <Link to="/list">Employee List</Link>
                     </Box>
